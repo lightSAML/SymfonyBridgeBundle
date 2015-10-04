a73e57614c9af9a971efb0bb6cf87deff9814965
Milos Tomic <tmilos@gmail.com>
2015-10-04 19:14:20 +0200

    config root name fix & session_processor dependency fix

--- src/LightSaml/SymfonyBridgeBundle/DependencyInjection/Configuration.php
+++ src/LightSaml/SymfonyBridgeBundle/DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('lightsaml');
+        $root = $treeBuilder->root('light_saml_symfony_bridge');
 
         $root->children()
             ->arrayNode('own')
