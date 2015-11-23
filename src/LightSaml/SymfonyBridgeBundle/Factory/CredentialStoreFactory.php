3d28bc1becb96c0d6070ccce3188f12417c93d80
Milos Tomic <tmilos@gmail.com>
2015-11-23 11:53:48 +0100

    remove symfony expressions and fix composer dependencies

---
+++ src/LightSaml/SymfonyBridgeBundle/Factory/CredentialStoreFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the LightSAML Symfony Bridge Bundle package.
+ *
+ * (c) Milos Tomic <tmilos@lightsaml.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace LightSaml\SymfonyBridgeBundle\Factory;
+
+use LightSaml\Store\Credential\CredentialStoreInterface;
+use LightSaml\Store\Credential\Factory\CredentialFactory;
+use LightSaml\Store\EntityDescriptor\EntityDescriptorStoreInterface;
+
+class CredentialStoreFactory
+{
+    public function build(
+        EntityDescriptorStoreInterface $idpEntityDescriptorStore,
+        EntityDescriptorStoreInterface $spEntityDescriptorStore,
+        $ownEntityId,
+        CredentialStoreInterface $ownCredentialStore,
+        array $extraCredentials = null
+    ) {
+        $factory = new CredentialFactory();
+
+        return $factory->build(
+            $idpEntityDescriptorStore,
+            $spEntityDescriptorStore,
+            $ownCredentialStore->getByEntityId($ownEntityId),
+            $extraCredentials
+        );
+    }
+}
---
+++ src/LightSaml/SymfonyBridgeBundle/Factory/OwnEntityDescriptorProviderFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the LightSAML Symfony Bridge Bundle package.
+ *
+ * (c) Milos Tomic <tmilos@lightsaml.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace LightSaml\SymfonyBridgeBundle\Factory;
+
+use LightSaml\Builder\EntityDescriptor\SimpleEntityDescriptorBuilder;
+use LightSaml\Credential\X509Credential;
+use LightSaml\Store\Credential\CredentialStoreInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class OwnEntityDescriptorProviderFactory
+{
+    public function build(
+        $ownEntityId,
+        RouterInterface $router,
+        $acsRouteName,
+        $ssoRouteName,
+        CredentialStoreInterface $ownCredentialStore
+    ) {
+        /** @var X509Credential[] $arrOwnCredentials */
+        $arrOwnCredentials = $ownCredentialStore->getByEntityId($ownEntityId);
+        $builder = new SimpleEntityDescriptorBuilder(
+            $ownEntityId,
+            $acsRouteName ? $router->generate($acsRouteName, [], RouterInterface::ABSOLUTE_URL) : null,
+            $ssoRouteName ? $router->generate($ssoRouteName, [], RouterInterface::ABSOLUTE_URL) : null,
+            $arrOwnCredentials[0]->getCertificate()
+        );
+    }
+}
